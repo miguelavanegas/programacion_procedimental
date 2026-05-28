919296e1cecd55907d0b629fc9b976d639e29d60
miguelavanegas <miguel.a.vanegas.a@gmail.com>
2026-05-28 17:14:18 -0500

    get_user_information : create js for cuestions

---
+++ restaurante/js/inventario.js
@@ -0,0 +1,4 @@
+let codigo = document.getElementById("codigo").value;
+let nombre_producto = document.getElementById("nombre_producto").value;
+let cantidad = document.getElementById("cantidad").value;
+let marca = document.getElementById("marca").value;
---
+++ restaurante/js/pedidos.js
@@ -0,0 +1,6 @@
+let pedidos = document.getElementById("pedidos").value;
+let numero = document.getElementById("numero").value;
+let producto = document.getElementById("producto").value;
+let cantidad = document.getElementById("cantidad").value;
+let direccion = document.getElementById("direccion").value;
+let pago = document.getElementById("pago").value;
