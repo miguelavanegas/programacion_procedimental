a67a5ad4aff0c6c75629bb01b7bafbc226b1ab87
miguelavanegas <miguel.a.vanegas.a@gmail.com>
2026-06-11 17:14:29 -0500

    validate_module_inventario : agree validates

--- restaurante/js/inventario.js
+++ restaurante/js/inventario.js
@@ -2,3 +2,22 @@ let codigo = document.getElementById("codigo").value;
 let nombre_producto = document.getElementById("nombre_producto").value;
 let cantidad = document.getElementById("cantidad").value;
 let marca = document.getElementById("marca").value;
+
+function validarFormulario() {
+    if (codigo == "" || nombre_producto == "" || cantidad == "" || marca === "") {
+        console.log("Por favor, complete todos los campos del formulario.");
+        return false;
+    }
+
+        function solonumeros() {
+            let regex = /^\d+$/;
+            return regex.test(codigo) && regex.test(cantidad);
+        }
+
+        function sololetras() {
+            let regex = /\d/;
+            return regex.test(nombre_producto) && regex.test(marca);
+        }
+
+    return true;
+}
